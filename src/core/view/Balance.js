dd47a88b0e671fe72d6c92a5050723f8216e72ec
anakzel <outmadness@gmail.com>
2022-06-08 11:44:10 +0300

    pixi fix

--- src/core/view/Balance.js
+++ src/core/view/Balance.js
@@ -2,7 +2,7 @@ import TextStyles from 'consts/TextStyles'
 import UserModel from 'core/models/UserModel'
 import ViewNotify from 'core/observers/notifications/ViewNotify'
 import ViewObserver from 'core/observers/ViewObserver'
-import { Container, Text } from 'Pixi.js'
+import { Container, Text } from 'pixi.js'
 function create() {
 	const container = new Container()
 	const textField = new Text('', TextStyles.balance)
